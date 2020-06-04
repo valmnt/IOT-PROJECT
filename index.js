a7f828580022a919e0c7f259d6f5f841e374cf61
Valmnt <valentin.mont@ynov.com>
2020-06-04 17:59:03 +0200

    mqtt connect fail

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-var mqtt = require('mqtt')
+var mqtt = require("mqtt");
+
+var client = mqtt.connect(
+    "mqtt://7q1q1p.messaging.internetofthings.ibmcloud.com",
+    (options = {
+        clientId: "A:7q1q1p:DTC:iot-project",
+        username: "a-7q1q1p-3y6xqarcyl",
+        password: "&_lHGw?qA(OR5ubnKe",
+        clean: true,
+    })
+);
+
+client.on("connect", function () {
+    console.log("connected" + client.connected);
+});
+
+client.end();
