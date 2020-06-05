af6606d7966fee19f2e18ef878fcba6e099aa079
MONT <valentin.mont@ynov.com>
2020-06-05 16:07:44 +0200

    buf

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
-/*var mqtt = require("mqtt");
-
-var client = mqtt.connect(
-    "mqtt://7q1q1p.messaging.internetofthings.ibmcloud.com",
-    (options = {
-        clientId: "A:7q1q1p:DTC:iot-project",
-        username: "a-7q1q1p-3y6xqarcyl",
-        password: "&_lHGw?qA(OR5ubnKe",
-        clean: true,
-    })
-);
-
-client.on("connect", function () {
-    console.log("connected" + client.connected);
-});
-
-client.end();*/
-
 var Client = require("ibmiotf");
-var appClientConfig = 
-    {
-        "org": "7q1q1p",
-        "id": "iot-project",
-        "auth-key": "a-7q1q1p-ziuj6dn3ci",
-        "auth-token": "YeboYWvn7Z*DUfYsoM",
-      }
-
+var appClientConfig = require("./application.json");
 
 var appClient = new Client.IotfApplication(appClientConfig);
 
 var connection = appClient.connect();
+console.log(connection);
 
 appClient.on("connect", function () {
-  console.log(connection);
+  console.log(appClientConfig);
+  /*setInterval(function function_name () {
+    appClient.publish('temp', 'json', '{"hum":' + 6 +'}');
+  },2000);*/
 });
-
-appClient.on("temp", function () {
-    console.log("temp");
-  });
